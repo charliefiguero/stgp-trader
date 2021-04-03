168df5cdb88fc995b6b2ac3bf695c266e7fea1ea
Charles Figuero <clewisfiguero@gmail.com>
2021-04-03 16:06:59 +0100

    refactor

--- GP_Entity.py
+++ GP_Entity.py
@@ -1,12 +1,17 @@
 import sys
+import os
+import inspect
 import random
 import math
 import operator
 import numpy
 
 from deap import gp, creator, base, tools, algorithms
+
 from BSE2_msg_classes import Assignment, Order, ExchMsg
 from BSE2_trader_agents import Trader
+from BSE2_Entity import Entity
 
-class GP_Trader_Tree(Trader):
-    
+class STGP_Entity(Entity):
+    Trader()
+    pass
