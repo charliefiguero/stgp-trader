7b7f4ec299246f1744e26990c1a6b62a873af5ac
Charles Figuero <clewisfiguero@gmail.com>
2021-04-08 23:45:33 +0100

    added init population funciton to stgp entity and renamed gptrader to stgp trader

--- STGP_Entity.py
+++ STGP_Entity.py
@@ -12,21 +12,38 @@ import pygraphviz as pgv
 from BSE2_msg_classes import Assignment, Order, ExchMsg
 from BSE2_trader_agents import Trader
 from BSE2_Entity import Entity
+from STGP_Trader import STGP_Trader
 
 
 def if_then_else(inputed, output1, output2):
     return output1 if inputed else output2
+
+def draw_expr(expr):
+    nodes, edges, labels = gp.graph(expr)
+
+    ### Graphviz Section ###
+    g = pgv.AGraph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    g.layout(prog="dot")
+
+    for i in nodes:
+        n = g.get_node(i)
+        n.attr["label"] = labels[i]
+
+    # saves to tree.pdf
+    now = datetime.datetime.now()
+    # print(f"Current time: {now}\n")
+    g.draw(f"trees/tree {now}.pdf")
 
 
 class STGP_Entity(Entity):
 
     def __init__(self, id, init_balance):
         super().__init__(id, init_balance, 0, {})
-
         self.pset, self.toolbox = self.create_deap_toolbox_and_pset()
 
-        self.pop_size = 1
-        self.time_since_last_trader_eval = 0
+        self.exprs = []
 
     def create_deap_toolbox_and_pset(self):
         # initialise pset
@@ -53,34 +70,32 @@ class STGP_Entity(Entity):
         toolbox = base.Toolbox()
         toolbox.register("expr", gp.genFull, pset=pset, min_=1, max_=3)
         toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
         return pset, toolbox
+
+    def init_traders(self, n, balance, time):
+        self.exprs = self.toolbox.population(n)
+
+        for count, expr in enumerate(self.exprs):
+            trading_function = gp.compile(gp.PrimitiveTree(expr), self.pset)
+            self.traders.append(STGP_Trader(count, balance, time, trading_function))
         
 
 if __name__ == "__main__":
 
-    # create tree
+    
     e = STGP_Entity(0, 100)
-    expr = e.toolbox.individual()
-    tree = gp.PrimitiveTree(expr)
+    e.init_traders(10, 100, datetime.datetime.now())
 
-    print(tree)
-    nodes, edges, labels = gp.graph(expr)
+    # exprs = e.toolbox.population(10)
+    # draw_expr(exprs[0])
+    # a_tree = gp.PrimitiveTree(exprs[0])
+    # print(a_tree)
+    # function = gp.compile(a_tree, e.pset)
+    # print(function(1))
 
-    ### Graphviz Section ###
-    g = pgv.AGraph()
-    g.add_nodes_from(nodes)
-    g.add_edges_from(edges)
-    g.layout(prog="dot")
 
-    for i in nodes:
-        n = g.get_node(i)
-        n.attr["label"] = labels[i]
-
-    # saves to tree.pdf
-    now = datetime.datetime.now()
-    print(f"Current time: {now}\n")
-    g.draw(f"trees/tree {now}.pdf")
-
-    output = gp.compile(tree, e.pset)
-    print(output(1))
+    # TODO create traders
+    # TODO initialise traders in STGP_Entity
+    # TODO register traders on the exchange
---
+++ STGP_Trader.py
@@ -0,0 +1,28 @@
+""" GP_Entity will initialise multiple of these and give them each an improvement function dictated by the tree of the individual."""
+
+import sys
+import random
+
+from BSE2_msg_classes import Assignment, Order, ExchMsg
+from BSE2_trader_agents import Trader
+
+
+class STGP_Trader(Trader):
+    
+    def __init__(self, tid, balance, time, trading_func):
+        super.__init__("stgp_trader", tid, balance, time)
+        self.trading_func = trading_func
+
+    def getorder(self, time, countdown, lob, verbose):
+        pass
+
+    def respond(self, time, lob, trade, verbose):
+        pass
+
+    def ema_ind(self):
+        """ exponential moving average indicator for the trader """
+        ema = 1
+        return 1
+    
+
+
