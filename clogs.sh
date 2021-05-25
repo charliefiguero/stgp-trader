ada0aad0f1b9477c35a52b446753d1986b05e2dc
Charles Figuero <clewisfiguero@gmail.com>
2021-05-25 01:10:18 +0100

    added clogs comment

--- clogs.sh
+++ clogs.sh
@@ -3,6 +3,7 @@ rm Test00blotters.csv
 rm Test00prices.csv
 rm Test00tapes.csv
 
+# removes all files from dir except the .gitkeep
 find ./stgp_csvs/gen_records/ ! -name '.gitkeep' -type f -exec rm -f {} +
 find ./stgp_csvs/hall_of_fame/ ! -name '.gitkeep' -type f -exec rm -f {} +
 find ./stgp_csvs/improvements/ ! -name '.gitkeep' -type f -exec rm -f {} +
